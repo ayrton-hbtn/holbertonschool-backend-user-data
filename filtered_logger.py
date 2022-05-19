5043f1e31ecbe614833c41d3d07333e57ab196d5
ayrton-hbtn <3282@holbertonschool.com>
2022-05-18 23:34:23 -0300

    formatter with filtered data

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Personal data"""
 
+import logging
 import re
 
 
@@ -10,12 +11,30 @@ def filter_datum(fields: list, redaction: str,
     filters the data in message that match the field
     in fields list, and replace it's 'value' with redaction
     """
-    for field in fields:
-        for msg_field in message.split(separator)[:-1:]:
-            msg_field = msg_field.split('=')
-            parameter = msg_field[0]
-            value = msg_field[1]
-            if field == parameter:
-                message = re.sub(value, redaction, message)
+    for field in message.split(separator):
+        if field.split('=')[0] in fields:
+            message = re.sub(field.split('=')[1], redaction, message)
 
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """inherits init from Formatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        uses the format() method from parent and
+        applies and extra filter with filter_datum()
+        """
+        log = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
