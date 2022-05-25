ccf7dd8703b3cf830f97e774ef198dd68d8815b3
ayrton-hbtn <3282@holbertonschool.com>
2022-05-24 23:48:43 -0300

    define not auth routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,15 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ TO-DO implementation
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        for excl_path in excluded_paths:
+            if path in excl_path:
+                return False
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """ TO-DO implementation
