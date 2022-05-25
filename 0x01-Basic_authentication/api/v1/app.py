4e9d111bea93e3fc675db087235f92c6b96e057b
ayrton-hbtn <3282@holbertonschool.com>
2022-05-25 19:10:41 -0300

    fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-auth = None
 auth = getenv("AUTH_TYPE", None)
+
 if auth:
     if auth == 'basic_auth':
         from api.v1.auth.basic_auth import BasicAuth
@@ -53,11 +53,12 @@ def validate_auth() -> None:
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
         ]
-    if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+    if auth:
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 if __name__ == "__main__":
