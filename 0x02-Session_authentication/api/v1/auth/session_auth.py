05c66a7472cae135d71ef72804ec269c211bc5dd
ayrton-hbtn <3282@holbertonschool.com>
2022-05-26 01:53:53 -0300

    implement logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -38,3 +38,19 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
 
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """
+        """
+        if not request:
+            return False
+
+        session_id = self.session_cookie(request)
+
+        if not session_id:
+            return False
+        if not self.user_id_for_session_id(session_id):
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
