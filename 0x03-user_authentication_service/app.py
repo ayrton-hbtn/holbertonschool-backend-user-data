1646f92e18868c5207b006e7b00390f619496273
ayrton-hbtn <3282@holbertonschool.com>
2022-06-02 23:42:27 -0300

    password reset

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,5 +80,19 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password():
+    """updates a user password"""
+    email = request.form["email"]
+    reset_token = request.form["reset_token"]
+    new_password = request.form["new_password"]
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,14 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
 
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pwd = _hash_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=hashed_pwd,
+                                 reset_token=None)
+        except Exception:
+            raise ValueError("invalid reset token")
