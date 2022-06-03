340e24d6e528d9b6c850719832d84e95013cb244
ayrton-hbtn <3282@holbertonschool.com>
2022-06-02 22:51:32 -0300

    create session for user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """ Flask Application """
 
-from multiprocessing.sharedctypes import Value
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 
 app = Flask(__name__)
@@ -29,6 +28,21 @@ def users():
             })
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """User login"""
+    email = request.form["email"]
+    password = request.form["password"]
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    res = jsonify({"email": "<user email>", "message": "logged in"})
+    res.set_cookie("session_id", session_id)
+    return res
 
 
 if __name__ == "__main__":
